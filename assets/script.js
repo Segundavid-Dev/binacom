6942bf45ab6a8f20c12d9a9a2f4be60f6e80ad87
Segun David <segdavid03@gmailcom>
2024-11-27 18:28:45 +0100

    feat: toggle menu links open and close

--- assets/script.js
+++ assets/script.js
@@ -2,10 +2,16 @@ const hamburgerMenu = document.querySelector(".hamburger__menu")
 const hamburgerIcon = document.querySelector(".hamburger__icon")
 const iconClose = document.querySelector(".ri-close-large-line")
 const iconOpen = document.querySelector(".ri-menu-2-line")
+const menuLinks = document.querySelector(".menu__links")
 
 
 // open and close hamburger icon
 hamburgerIcon.addEventListener("click", function(){
     iconClose.classList.toggle("hidden")
+    menuLinks.classList.toggle("open")
     iconOpen.classList.toggle("hidden")
+
+
+    // show menu links
+    
 })
